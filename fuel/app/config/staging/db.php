a3f1f99aeee9e24a7dae72a85a6a63727598dcdb
Scott Sowers <scott.sowers@live.com>
2014-03-26 22:38:11 -0700

    Signed-off-by: Scott Sowers <scott.sowers@live.com>

--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_staging',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'dsn'        => 'mysql:host=http://ec2-54-201-182-243.us-west-2.compute.amazonaws.com;dbname=ciscoquizss',
+			'username'   => 'ytk',
+			'password'   => 'ytk',
 		),
 	),
 );
