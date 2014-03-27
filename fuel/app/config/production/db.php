15234ff7c8f907f36167f06fe35e34b45682892d
Scott Sowers <scott.sowers@live.com>
2014-03-26 23:04:46 -0700

    Changed MySQL host to localhost

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=http://ec2-54-201-182-243.us-west-2.compute.amazonaws.com;dbname=ciscoquizss',
+			'dsn'        => 'mysql:host=localhost;dbname=ciscoquizss',
 			'username'   => 'ytk',
 			'password'   => 'ytk',
 		),
